9d4d31e2be855bb9c4e0352be48dc546da1cdf0e
albarkan <55287984+albarkan@users.noreply.github.com>
2019-11-03 15:38:16 +0300

    2

--- ege25/ege25/25.1.h
+++ ege25/ege25/25.1.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <iostream>
-
-using namespace std;
-
-void arrayIn(int* array, int size);
-
-void arrayOut(int* array, int size);
-
 void task7770();
 
 void task7797();
@@ -19,5 +11,7 @@ void task9209();
 void task9811();
 
 void task2914();
+
+int task2913();
 
 
