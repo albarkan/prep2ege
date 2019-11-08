a1f941ab4dd3cc193bd01f27a1a89ec7fa50d1c3
albarkan <55287984+albarkan@users.noreply.github.com>
2019-11-08 23:06:19 +0300

    3

---
+++ 8.11.2019/8.11.2019/25.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int task6349();
+int task7426();
+int task5068();
+int task6790();
+int task4957();
---
+++ 8.11.2019/8.11.2019/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "25.h"
+
+using namespace std;
+
+int main()
+{
+	task4957();
+
+	return 0;
+}
