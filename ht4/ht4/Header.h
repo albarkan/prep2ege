5b9f456666a49a03cc14a49af2facfd8d947ffb9
albarkan <55287984+albarkan@users.noreply.github.com>
2019-12-12 15:21:21 +0300

    g

---
+++ ht4/ht4/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+
+ int task4569();
+ int task7426();
+ int task5321();
+ int task6349();
+ int task6334();
+ int task6512();
+ int task4860();
---
+++ ht4/ht4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+int main()
+{
+	task4860();
+	return 0;
+}
