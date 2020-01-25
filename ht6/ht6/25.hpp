64aaaa92710fe59e7c6efdc17cb44be4c4148c5e
albarkan <55287984+albarkan@users.noreply.github.com>
2020-01-25 21:07:58 +0300

    я

--- ht6/ht6/25.hpp
+++ ht6/ht6/25.hpp
@@ -1,7 +1,7 @@
-
 int task6588();
 int task9706();
 int task9376();
 int task6936();
 int task7709();
-int task2918();
+int task9177();
+int task10488();
--- ht6/ht6/main.cpp
+++ ht6/ht6/main.cpp
@@ -6,5 +6,5 @@ using namespace std;
 
 int main()
 {
-    task6936();
+    task10488();
 }
