cf34c9ded010594bf242c1079f5e340345948329
albarkan <55287984+albarkan@users.noreply.github.com>
2020-03-09 17:01:47 +0300

    commit 9.03

---
+++ practisingTest1(24-27)/practisingTest1(24-27)/aegy.h
@@ -0,0 +1,16 @@
+//
+//  aegy.h
+//  practisingTest1(24-27)
+//
+//  Created by barkanoid on 02.03.2020.
+//  Copyright © 2020 barkanoid. All rights reserved.
+//
+
+#ifndef aegy_h
+#define aegy_h
+
+
+#endif /* aegy_h */
+
+int task5256();
+int task2920();
---
+++ practisingTest1(24-27)/practisingTest1(24-27)/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "aegy.h"
+
+using namespace std;
+
+int main()
+{
+    task2920();
+    return 0;
+}
+
