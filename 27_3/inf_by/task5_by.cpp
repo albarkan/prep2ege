cf6e3bc55a6becba1f480c1f920764e55a16a561
albarkan <55287984+albarkan@users.noreply.github.com>
2020-06-18 20:27:15 +0300

    b

---
+++ 27_3/inf_by/task5_by.cpp
@@ -0,0 +1,39 @@
+/* R - наибольшее произведение двух членов последовательности, кратное 6
+ количество чисел вводится с клавиатуры*/
+
+#include <iostream>
+
+using namespace std;
+
+int task5_by()
+{
+    int k = 0, kr2 = 0, kr3 = 0, kr6 = 0, maxany = 0, R = 0;
+       int size = 0, seq = 0;
+       
+       cin >> size;
+       
+       for (int i = 0; i < size; i++) {
+           cin >> seq;
+           
+           if (seq % 2 == 0 && seq > kr2 && seq % 6 != 0) {
+               kr2 = seq;
+           }
+           else if (seq % 3 == 0 && seq > kr3 && seq % 6 != 0) {
+               kr3 = seq;
+           }
+           else if (seq % 6 == 0 && seq > kr6) {
+               kr6 = seq;
+           }
+           else if (seq > maxany) {
+               maxany = seq;
+           }
+           }
+    if (kr2 * kr3 > 0) {
+        R = kr2 * kr3;
+    }
+    else if (kr6 * maxany > 0 && kr6 * maxany > R) {
+        R = kr6 * maxany;
+    }
+    else if (kr6 * kr2 > 0 && kr6 * kr2 > R) {
+    R = kr6 * kr2;
+    }
